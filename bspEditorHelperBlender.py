10ac5c1ebf832ff2af9e6d73468cd91bd61d1ea5
Patatifique <Patatifique@icloud.com>
2024-08-17 14:08:28 +0200

    cleanup

--- bspEditorHelperBlender.py
+++ bspEditorHelperBlender.py
@@ -3,20 +3,20 @@ bl_info = {
     "blender": (4, 2, 0),  # Updated to Blender 4.2
     "category": "Object",
     "author": "Patatifique",
-    "description": "Adds dynamically named render flags and armature flags to selected objects and bones.",
+    "description": "Plugin for Blender 4.2, various function to help working with ghost master.",
 }
 
 import bpy
 
 def get_next_render_flag_index(obj):
-    """Get the next available render flag index for the given object."""
+    # Get the next available render flag index for the given object.
     index = 1
     while f"RENDERFLAGS{index}" in obj:
         index += 1
     return index
 
 class OBJECT_OT_AddLightableProperty(bpy.types.Operator):
-    """Add the next available RENDERFLAGS property with the value RUNTIME_LIGHTABLE"""
+    """Set RUNTIME_LIGHTABLE Render Flag"""
     bl_idname = "object.add_lightable_property"
     bl_label = "Lightable"
     bl_options = {'REGISTER', 'UNDO'}
@@ -29,7 +29,7 @@ class OBJECT_OT_AddLightableProperty(bpy.types.Operator):
         return {'FINISHED'}
 
 class OBJECT_OT_AddDoubleSidedProperty(bpy.types.Operator):
-    """Add the next available RENDERFLAGS property with the value DOUBLE_SIDED"""
+    """Set DOUBLE_SIDED Render Flag"""
     bl_idname = "object.add_double_sided_property"
     bl_label = "Double Sided"
     bl_options = {'REGISTER', 'UNDO'}
@@ -42,7 +42,7 @@ class OBJECT_OT_AddDoubleSidedProperty(bpy.types.Operator):
         return {'FINISHED'}
 
 class ARMATURE_OT_SetHeadbone(bpy.types.Operator):
-    """Rename the selected bone to MDL-jnt-HEADBONE"""
+    """Set selected bone as MDL-jnt-HEADBONE"""
     bl_idname = "armature.set_headbone"
     bl_label = "Set Headbone"
     bl_options = {'REGISTER', 'UNDO'}
@@ -57,7 +57,7 @@ class ARMATURE_OT_SetHeadbone(bpy.types.Operator):
         return {'FINISHED'}
 
 class BSP_EDITOR_HELPER_PT_Panel(bpy.types.Panel):
-    """Creates a Panel in the Top Bar next to Item, Tool, View"""
+    # Creates Panel
     bl_label = "BSP Editor Helper"
     bl_idname = "BSP_EDITOR_HELPER_PT_panel"
     bl_space_type = 'VIEW_3D'
@@ -98,6 +98,3 @@ def unregister():
     bpy.utils.unregister_class(OBJECT_OT_AddDoubleSidedProperty)
     bpy.utils.unregister_class(ARMATURE_OT_SetHeadbone)
     bpy.utils.unregister_class(BSP_EDITOR_HELPER_PT_Panel)
-
-if __name__ == "__main__":
-    register()
